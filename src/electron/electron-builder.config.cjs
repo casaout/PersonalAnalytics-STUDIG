8e82735079865cddf66cfc365d4019dcb7c8b62e
André Meyer <casaout@users.noreply.github.com>
2024-09-09 20:09:16 +0200

    updated owner and repo of electron-builder-config
    https://github.com/HASEL-UZH/D2USP/issues/1

--- src/electron/electron-builder.config.cjs
+++ src/electron/electron-builder.config.cjs
@@ -9,8 +9,8 @@ module.exports = {
   files: ['dist', 'dist-electron'],
   publish: {
     provider: 'github',
-    owner: 'HASEL-UZH',
-    repo: 'PersonalAnalytics'
+    owner: 'casaout',
+    repo: 'PersonalAnalytics-STUDIG'
   },
   mac: {
     artifactName: '${productName}-Mac-${version}-Installer.${ext}',
